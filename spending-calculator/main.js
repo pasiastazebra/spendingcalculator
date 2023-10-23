edec7a7bb8119936f1d42baeefd418191998e89c
Konrad <vobis08gw@gmail.com>
2023-10-23 19:34:54 +0200

    HTML done

--- spending-calculator/main.js
+++ spending-calculator/main.js
@@ -1,24 +1,13 @@
-import './style.scss'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import '/src/style.scss';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const modal = document.querySelector('.modal');
+const showButton = document.getElementById('showModal');
+const closeButton = document.getElementById('closeModal');
 
-setupCounter(document.querySelector('#counter'))
+showButton.addEventListener('click', () => { 
+  modal.showModal();
+ });
+
+ closeButton.addEventListener('click', () => {
+  modal.close();
+ });
